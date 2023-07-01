9e8882a0ece3fee1bb1b3e36d576c99037d35bf7
Phol <Pholibertlim@gmail.com>
2023-07-01 23:52:04 +0800

    feat: initial commit for scheduler service

---
+++ server/Scheduler-Service/app.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+
+const app = express();
+
+//Third party middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(morgan('dev'));
+
+
+export default app
